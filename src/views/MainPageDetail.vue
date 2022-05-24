acb5ac7b4d90c112d4f474b00788cf40a6a867ca
joyfulLife <joys0719@gmail.com>
2022-05-24 18:29:37 +0900

    push 확인

--- src/views/MainPageDetail.vue
+++ src/views/MainPageDetail.vue
@@ -25,7 +25,8 @@
           <b-card-text>
             Some quick example text to build on the <em>card title</em> and make up the bulk of the card's
             content.Some quick example text to build on the <em>card title</em> and make up the bulk of the card's
-            content.
+            content.    
+            
           </b-card-text>
 
           <b-card-text>A second paragraph of text in the card.</b-card-text>
